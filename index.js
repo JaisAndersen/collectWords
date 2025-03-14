83bf306e15c5f2dd362da208c2e42caf7c69376d
Jais Andersen <jais.h.sorensen@gmail.com>
2025-03-14 14:35:18 +0100

    Play

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 Vue.createApp({
     data() {
         return {
-
-        }
+            name: "",
+            names: [],       
+        };
     },
     methods: {
-
+        saveName() {
+            if (this.name.trim() !== '') {
+                this.names.push(this.name);
+                this.name = '';
+                this.showTable = false;
+                this.message = '';
+            }
+        },
+        showNames() {
+            if (this.names.length > 0) {
+                this.showTable = true;
+            } else {
+                this.message = 'No names to display';
+            }
+        },
+        clearNames() {
+            this.names = [];
+        }
     }
 }).mount("#app");
